2439f895e028582aa4ad5959de214787552f6ebd
yochatapa <yochatapa@gmail.com>
2025-04-22 18:20:05 +0900

    FIX: 페이지 이동 시, 0,0으로 이동되도록 수정

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -22,6 +22,24 @@ const router = createRouter({
             component: RegisterView,
         }
     ],
+    scrollBehavior(to, from, savedPosition) {
+        // savedPosition은 뒤로가기/앞으로가기 버튼으로 이동했을 때
+        // 브라우저에 의해 저장된 스크롤 위치입니다.
+        if (savedPosition) {
+          return savedPosition; // 저장된 위치가 있으면 그 위치로 이동
+        } else {
+          // 저장된 위치가 없으면 (새로운 경로로 이동할 때)
+          // 항상 페이지 맨 위 (0, 0)로 스크롤
+          return { top: 0, left: 0 };
+        }
+        // 만약 URL에 해시(#)가 있다면 해당 요소로 스크롤되게 할 수도 있습니다.
+        if (to.hash) {
+          return {
+            el: to.hash, // 해시에 해당하는 요소를 찾아서 스크롤
+            behavior: 'smooth', // 부드럽게 스크롤 (선택 사항)
+          };
+        }
+      },
 })
 
 export default router
