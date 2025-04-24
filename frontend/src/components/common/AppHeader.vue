6116a7b23bfd84ae4c374d2f8eb62ed2e65574de
Jinseob <jinseob@hangang.com>
2025-04-24 16:31:11 +0900

    FEAT: 로그인 시, 헤더 오른쪽 사용자 아이콘 + 팝업으로 변경

--- frontend/src/components/common/AppHeader.vue
+++ frontend/src/components/common/AppHeader.vue
@@ -13,24 +13,63 @@ const logout = () => userStore.logout()
 
 <template>
     <v-app-bar flat height="64" class="bg-white border-b">
-        <!-- 중앙 정렬된 컨테이너 -->
         <v-container class="d-flex align-center justify-space-between" style="max-width: 1200px;">
-        <!-- 로고 및 타이틀 -->
-        <div class="d-flex align-center cursor-pointer" @click="router.push('/')">
-            <v-icon size="28" color="primary" class="mr-2">mdi-baseball</v-icon>
-            <span class="text-h6 font-weight-bold text-primary">KBO Fantasy League</span>
-        </div>
+            <!-- 로고 및 타이틀 -->
+            <div class="d-flex align-center cursor-pointer" @click="router.push('/')">
+                <v-icon size="28" color="primary" class="mr-2">mdi-baseball</v-icon>
+                <span class="text-h6 font-weight-bold text-primary">KBO Fantasy League</span>
+            </div>
 
-        <!-- 우측 로그인 영역 -->
-        <div class="d-flex align-center">
-            <template v-if="isLoggedIn">
-                <v-btn variant="text" class="text-body-1 text-black" @click="">{{ user?.nickname }}</v-btn>
-                <v-btn variant="text" class="text-black" @click="logout">로그아웃</v-btn>
-            </template>
-            <template v-else>
-                <v-btn variant="flat" color="primary" @click="goToLogin">로그인</v-btn>
-            </template>
-        </div>
+            <!-- 우측 사용자 메뉴 -->
+            <div class="d-flex align-center">
+                <template v-if="isLoggedIn">
+                    <v-menu offset-y>
+                        <template #activator="{ props }">
+                            <v-avatar
+                                v-bind="props"
+                                size="36"
+                                class="cursor-pointer"
+                                style="border: 2px solid #ccc;"
+                            >
+                                <v-img
+                                    v-if="user?.imageUrl"
+                                    :src="user.imageUrl"
+                                    alt="User Image"
+                                />
+                                <v-icon v-else size="28" color="#666">mdi-account-circle</v-icon>
+                            </v-avatar>
+                        </template>
+                        <v-card min-width="200" class="pa-2">
+                            <div class="px-2 py-1">
+                                <div class="text-subtitle-1 font-weight-medium">{{ user?.nickname }}</div>
+                                <div class="text-caption text-grey-darken-1">{{ user?.email }}</div>
+                            </div>
+                            <v-divider class="my-2"></v-divider>
+                            <v-btn
+                                variant="text"
+                                color="primary"
+                                block
+                                @click="router.push('/profile')"
+                            >
+                                내 정보 관리
+                            </v-btn>
+                            <v-btn
+                                variant="text"
+                                color="error"
+                                block
+                                class="mt-1"
+                                @click="logout"
+                            >
+                                <v-icon start size="18">mdi-logout</v-icon>
+                                로그아웃
+                            </v-btn>
+                        </v-card>
+                    </v-menu>
+                </template>
+                <template v-else>
+                    <v-btn variant="flat" color="primary" @click="goToLogin">로그인</v-btn>
+                </template>
+            </div>
         </v-container>
     </v-app-bar>
 </template>
