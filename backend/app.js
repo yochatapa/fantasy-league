28a2ff3ffe30815e8d4f7369c17e3504b837f30f
Jinseob <jinseob@hangang.com>
2025-04-25 16:09:23 +0900

    TEST: 하나의 프로젝트로 배포 테스트1

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import cookieParser from 'cookie-parser'
 import testRoutes from './routes/testRoutes.js';
 import usersRoutes from './routes/usersRoutes.js';
@@ -10,17 +12,30 @@ dotenv.config();
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 // 미들웨어 설정
 app.use(cors({
     origin: process.env.FRONTEND_URL, // 정확한 origin 명시
     credentials: true,               // 쿠키 포함 허용
 }));
 app.use(express.json());
-app.use(cookieParser())
+app.use(cookieParser());
+
+if(process.env.SERVE_FRONTEND === 'true'){
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+}
 
 // 라우팅 설정
 app.use('/api/test' , testRoutes);
 app.use('/api/users', usersRoutes);
 app.use('/api/auth' , authRoutes);
+
+if(process.env.SERVE_FRONTEND === 'true'){
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+    });
+}
 
 export default app;
