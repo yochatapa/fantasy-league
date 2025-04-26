0cecfe19b88a7e09179055b9255dfb7cddd40483
jinseob <yochatapa@gmail.com>
2025-04-26 15:40:19 +0900

    FEAT: window 기본 alert, confirm 오버라이드

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,10 +8,22 @@ import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify';
 
+import { useAlertStore } from '@/stores/useAlertStore';
+
 const app = createApp(App)
 
 app.use(vuetify)
 app.use(createPinia())
 app.use(router)
+
+const alertStore = useAlertStore();
+
+window.alert = (message, type) => {
+    alertStore.Alert(message, type);
+};
+
+window.confirm = async (message) => {
+    return await alertStore.Confirm(message);
+};
 
 app.mount('#app')
